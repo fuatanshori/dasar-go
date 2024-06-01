b83445e5cd6c50b830a2c51bdd50a5b16aa217da
FUAT ANSHORI <anshorifuat12@gmail.com>
2024-06-01 16:42:19 +0700

    struct

--- 30-struct/struct.go
+++ 30-struct/struct.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
+type Number int
+type Mahasiswa struct{
+	Nama, Jurusan string
+	Nim, Umur Number
+}
 
 
 func main() {
-	
+	fuat := Mahasiswa{Nama: "Fuat Anshori", 
+	Jurusan: "Teknik Informatika", Nim: 20041066,
+	Umur: 24,
+	}
+	fmt.Println(fuat)
 }
+
